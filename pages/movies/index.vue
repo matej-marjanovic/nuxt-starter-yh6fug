eb6969eaa48c9f6de39ac117a7b8f378ee05c194
Matej Marjanovic <matej@outofgalaxy.com>
2022-10-19 15:08:59 +0200

    Fetch data with $fetcg

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,10 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const query = ref("");
+const movies = ref([]);
+async function search() {
+  const {Search} = await $fetch(`http://www.omdbapi.com/?apikey=5eec9741&s=${query.value}`)
+  movies.value = Search
+}
+</script>
 
 <template>
-  <form>
-    <input type="text" />
+  <form @submit.prevent="search">
+    <input type="text" v-model="query" />
     <button>Search</button>
   </form>
+  <div v-for="movie in movies" :key="movie.imdbID">
+    Movie: {{ movie.Title }}
+  </div>
 </template>
 
 <style scoped></style>
