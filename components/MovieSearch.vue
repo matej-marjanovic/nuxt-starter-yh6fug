21baa0969430a1f4cb2d515e0c156d98e52eac7c
Matej Marjanovic <matej@outofgalaxy.com>
2022-10-19 17:01:33 +0200

    using useAsyncData

---
+++ components/MovieSearch.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const query = ref("batman");
+const movies = ref([]);
+async function search() {
+  const {Search} = await $fetch(`http://www.omdbapi.com/?apikey=5eec9741&s=${query.value}`)
+  movies.value = Search
+}
+search();
+</script>
+
+<template>
+<div>
+  <form @submit.prevent="search">
+    <input type="text" v-model="query" />
+    <button>Search</button>
+  </form>
+  <ul>
+    <li v-for="movie in movies" :key="movie.imdbID">
+    <NuxtLink :to="{ name: 'movies-id', params: {id: movie.imdbID} }">
+      <img :src="movie.Poster" :alt="movie.Title">
+      <div>
+        Movie: {{ movie.Title }}
+      </div>
+    </NuxtLink>
+    </li>
+  </ul>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,5 +1,16 @@
+<script setup>
+const route = useRoute()
+const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
+  return $fetch(`http://www.omdbapi.com/?apikey=5eec9741&i=${route.params.id}`)
+}, {
+  pick: ["Plot", "Title"],
+})
+console.log(data)
+
+</script>
+
 <template>
-  <div>
-    <h1>{{ $route.params.id }}</h1>
-  </div>
+  <pre>
+    {{ data }}
+  </pre>
 </template>
