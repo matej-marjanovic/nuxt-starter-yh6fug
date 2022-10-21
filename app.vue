87aa540eeb92797d111b88d18292ff3206edca15
Matej Marjanovic <matej@outofgalaxy.com>
2022-10-22 01:56:23 +0200

    Create Flexible Custom Layouts

--- app.vue
+++ app.vue
@@ -9,11 +9,9 @@ useHead({
 
 <template>
   <div>
-    <nav style="display: flex; gap: 10px">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/movies">Movies</NuxtLink>
-    </nav>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+<nav style="display: flex; gap: 10px">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      Test
+      <NuxtLink to="/login">Login</NuxtLink>
+    </nav>
+<slot></slot>
+</template>
