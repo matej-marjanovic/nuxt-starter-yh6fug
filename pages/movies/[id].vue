8e53e1b4e537ab2514183f233f10cf5ef1a20d91
Matej Marjanovic <matej@outofgalaxy.com>
2022-10-22 01:32:11 +0200

    Handle useFetch Errors

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,8 +1,19 @@
 <script setup>
 const route = useRoute()
+// For testing server-side error:
+// `https://httpbin.org/status/500`
+// `https://www.omdbapi.com/?apikey=5eec9741&i=${route.params.id}`
 const { data } = useFetch(`https://www.omdbapi.com/?apikey=5eec9741&i=${route.params.id}`, {
   pick: ["Plot", "Title"],
   key: `/movies/${route.params.id}`,
+  onResponse({request, response}) {
+    if (response._data.Error === "Incorrect IMDb ID.") {
+      showError({statusCode: 404, statusMessage: "Incorrect ID. Not found"});
+    }
+  },
+  onResponseError() {
+    showError({statusCode: 500, statusMessage: "Sorry. Looks like something went wrong on our server."});
+  }
 });
 
 console.log(data)
