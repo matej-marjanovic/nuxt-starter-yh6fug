164e87829508e2a3eecf8a7e4097a126be169485
Matej Marjanovic <matej@outofgalaxy.com>
2022-10-22 01:22:09 +0200

    https on individual movie data fetch

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,6 +1,6 @@
 <script setup>
 const route = useRoute()
-const { data } = useFetch(`http://www.omdbapi.com/?apikey=5eec9741&i=${route.params.id}`, {
+const { data } = useFetch(`https://www.omdbapi.com/?apikey=5eec9741&i=${route.params.id}`, {
   pick: ["Plot", "Title"],
   key: `/movies/${route.params.id}`,
 });
